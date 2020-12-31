ad67a2de0b6e9d4e065b0ec909942c140da8df81
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2020-12-31 17:41:46 +0500

    useResults hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [searchApi, results, errorMsg] = useResults();
+
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log(`term was submitted`)}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>{term}</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
