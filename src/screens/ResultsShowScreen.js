491a3907bf137663e84174fc205570d034f1c5c5
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2020-12-31 18:53:42 +0500

    render fixes

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,47 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam(`id`);
+
+  const getResult = async (id) => {
+    try {
+      const response = await yelp.get(`/${id}`);
+      setResult(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>details</Text>
+      <Text>{result.name} </Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image source={{ uri: item }} style={styles.imageStyle} />;
+        }}
+      />
     </View>
   );
 };
 
 export default ResultsShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 300,
+    height: 200,
+  },
+});
